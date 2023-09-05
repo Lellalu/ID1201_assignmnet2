383ffeeb5c2296e82229f9344750b0c230a5430f
Lellalu <lella.lu14@outlook.com>
2023-09-05 16:34:17 +0200

    initialization

---
+++ src/main/java/se/kth/id1201/App.java
@@ -0,0 +1,13 @@
+package se.kth.id1201;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
